86c2ed57649f4df6a9e8ee5544536666c1d6fc70
mruzomberka311 <gmruz311@gmail.com>
2024-12-16 11:08:27 -0500

    update method in category dao complete

--- src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -1,10 +1,13 @@
 package org.yearup.data.mysql;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.yearup.data.CategoryDao;
 import org.yearup.models.Category;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -12,6 +15,8 @@ import java.util.List;
 @Component
 public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
 {
+
+    @Autowired
     public MySqlCategoryDao(DataSource dataSource)
     {
         super(dataSource);
@@ -27,27 +32,70 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
     @Override
     public Category getById(int categoryId)
     {
-        // get category by id
+        String query = "SELECT * FROM categories WHERE categoryId = ?";
+
+      try (Connection connection = getConnection();
+           PreparedStatement preparedStatement = connection.prepareStatement(query);){
+           ResultSet row = preparedStatement.executeQuery();
+           mapRow(row);
+
+
+      }catch (Exception e){
+          System.out.printf("An error has occurred");
+          e.printStackTrace();
+      }
         return null;
     }
 
     @Override
     public Category create(Category category)
     {
-        // create a new category
+        String query = "INSERT INTO categories (category_id, name, description) VALUES (?,?,?)";
+        try (Connection connection = getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(query);){
+
+
+        }catch (Exception e){
+            System.out.println("An error has occurred");
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
     public void update(int categoryId, Category category)
     {
-        // update category
+       String query = "UPDATE categories SET category_id = ?, name = ?, description = ?";
+       try  (Connection connection = getConnection();
+       PreparedStatement preparedStatement = connection.prepareStatement(query);){
+       int row = preparedStatement.executeUpdate();
+       if (row == 0){
+           throw new SQLException("Insert failed, no rows were affected");
+       }
+
+
+       }catch (Exception e){
+           System.out.println("An error has occurred");
+           e.printStackTrace();
+       }
     }
 
     @Override
     public void delete(int categoryId)
     {
-        // delete category
+        String query = "DELETE FROM categories WHERE category_id = ?";
+        try (Connection connection = getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(query);){
+            preparedStatement.setInt(1, categoryId);
+           int rows = preparedStatement.executeUpdate();
+           if (rows == 0){
+               throw new SQLException("Delete failed, no rows were affected.");
+           }
+
+        }catch (Exception e){
+            System.out.println("An error has occurred");
+            e.printStackTrace();
+        }
     }
 
     private Category mapRow(ResultSet row) throws SQLException
